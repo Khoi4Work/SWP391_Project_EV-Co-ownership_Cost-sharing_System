96e3d06dcc5bc9d180fc4d39e6a3e4d8f787f593
LAPTOP-BC2P92Q8\ASUS <khoimapu2k5@gmail.com>
2025-09-16 21:50:32 +0700

    asd

---
+++ App/src/main/java/khoindn/swp391/be/app/controller/UserController.java
@@ -0,0 +1,42 @@
+package khoindn.swp391.be.app.controller;
+
+import khoindn.swp391.be.app.pojo.Users;
+import khoindn.swp391.be.app.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/Users")
+public class UserController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Users>> getUserById(@PathVariable int id) {
+        return ResponseEntity.ok(iUserService.findUserById(id));
+    }
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Users> addUser(@RequestBody Users users) {
+        return ResponseEntity.ok(iUserService.addUser(users));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Users> updateUser(@RequestBody Users users,
+                                            @PathVariable int id) {
+        return ResponseEntity.ok(iUserService.updateUser(users, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable int id) {
+        iUserService.deleteUser(id);
+        return ResponseEntity.ok("Delete successfully");
+    }
+}
---
+++ App/src/main/java/khoindn/swp391/be/app/repository/IUserRepository.java
@@ -0,0 +1,7 @@
+package khoindn.swp391.be.app.repository;
+
+import khoindn.swp391.be.app.pojo.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<Users, Integer> {
+}
