5816eb9c506320cc8d5e04e291407ed832d73617
LAMVANTUAN\ADMIN <tlamvantuan@gmail.com>
2025-09-17 15:23:49 +0700

    Commit again Feature Login

---
+++ App/src/main/java/khoindn/swp391/be/app/repository/IAuthenticationRepository.java
@@ -0,0 +1,12 @@
+package khoindn.swp391.be.app.repository;
+
+import khoindn.swp391.be.app.pojo.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IAuthenticationRepository extends JpaRepository<Users, Integer> {
+    Optional<Users> findByEmailAndPassword(String email, String password);
+
+
+}
