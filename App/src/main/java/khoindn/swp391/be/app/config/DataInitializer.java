aebcf0ac5176a372585c63d3502e6fec4d3cf23f
Nguyen Khoi <nguyenkhoiforwork2005@gmail.com>
2025-10-03 23:08:12 +0700

    Test webhook

--- App/src/main/java/khoindn/swp391/be/app/config/DataInitializer.java
+++ App/src/main/java/khoindn/swp391/be/app/config/DataInitializer.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 @Component
 public class DataInitializer implements CommandLineRunner {
 
-    //Test webhook
     @Autowired
     private UserRoleService userRoleService;
 
