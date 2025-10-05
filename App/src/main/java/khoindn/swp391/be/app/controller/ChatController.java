cc3a2efbc2fcefdfba74dc3d9d6055700ae0b77f
Nguyen Khoi <nguyenkhoiforwork2005@gmail.com>
2025-10-05 12:36:33 +0700

    I commit at 12:35 add security requirement cho chatController

--- App/src/main/java/khoindn/swp391/be/app/controller/ChatController.java
+++ App/src/main/java/khoindn/swp391/be/app/controller/ChatController.java
@@ -1,11 +1,13 @@
 package khoindn.swp391.be.app.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import khoindn.swp391.be.app.service.ChatGPTService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat")
+@SecurityRequirement(name = "api")
 public class ChatController {
 
     @Autowired
