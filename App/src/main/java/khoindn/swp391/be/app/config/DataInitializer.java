e9f3e704701223bf1cf06dd4400bcd46ef049d04
LAMVANTUAN\ADMIN <tlamvantuan@gmail.com>
2025-10-05 15:01:43 +0700

    test

--- App/src/main/java/khoindn/swp391/be/app/config/DataInitializer.java
+++ App/src/main/java/khoindn/swp391/be/app/config/DataInitializer.java
@@ -21,7 +21,7 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-// test
+// test again
 
         if (!userRoleService.existsByRoleName("user") ||
                 !userRoleService.existsByRoleName("co-owner") ||
