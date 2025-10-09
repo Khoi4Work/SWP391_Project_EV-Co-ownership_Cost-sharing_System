8ccac512c8de5d60033b80895f438877f5c1529f
LAMVANTUAN\ADMIN <tlamvantuan@gmail.com>
2025-10-09 22:08:43 +0700

    i commit at 10:08 done

--- App/src/main/java/khoindn/swp391/be/app/controller/GroupMemberController.java
+++ App/src/main/java/khoindn/swp391/be/app/controller/GroupMemberController.java
@@ -1,6 +1,7 @@
 package khoindn.swp391.be.app.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import khoindn.swp391.be.app.pojo.GroupMember;
 import khoindn.swp391.be.app.service.IGroupMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/groupMember")
 @SecurityRequirement(name = "api")
 public class GroupMemberController {
     @Autowired
     private IGroupMemberService groupMemberService;
+
     @GetMapping("/getByUserId")
-    public ResponseEntity<?> getGroupMembersByUserId(@RequestParam("userId") int userId) {
-        return ResponseEntity.ok(groupMemberService.findAllByUsersId(userId));
+    public ResponseEntity<List<GroupMember>> getGroupMembersByUserId(@RequestParam("userId") int userId) {
+        List<GroupMember> groupMember = groupMemberService.findAllByUsersId(userId);
+        return ResponseEntity.ok(groupMember);
     }
 }
