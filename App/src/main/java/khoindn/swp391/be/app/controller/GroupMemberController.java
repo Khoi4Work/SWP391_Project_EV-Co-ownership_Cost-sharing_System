ca7d738371f8b00be86d8784af7875e5e2be9242
Kaizzet <nguyenvdse184427@fpt.edu.vn>
2025-10-19 20:35:25 +0700

    I Commit fix addmember

--- App/src/main/java/khoindn/swp391/be/app/controller/GroupMemberController.java
+++ App/src/main/java/khoindn/swp391/be/app/controller/GroupMemberController.java
@@ -1,9 +1,14 @@
 package khoindn.swp391.be.app.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import khoindn.swp391.be.app.model.Request.AddMemberRequest;
+import khoindn.swp391.be.app.model.Response.GroupMemberResponse;
 import khoindn.swp391.be.app.pojo.GroupMember;
 import khoindn.swp391.be.app.service.IGroupMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,19 +36,31 @@ public class GroupMemberController {
     }
 
     // ---------------------- NEW CODE: Add member to group ----------------------
-    @PostMapping("/add")
-    public ResponseEntity<GroupMember> addMember(
+    @PostMapping(
+            path = "/add",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<GroupMemberResponse> addMember(
             @RequestParam("groupId") int groupId,
-            @RequestBody GroupMember request
-    ) {
+            @Valid @RequestBody AddMemberRequest req) {
+
         GroupMember saved = groupMemberService.addMemberToGroup(
-                groupId,
-                request.getUsers().getId(),
-                request.getRoleInGroup(),
-                request.getOwnershipPercentage()
+                groupId, req.getUserId(), req.getRoleInGroup(), req.getOwnershipPercentage());
+
+        GroupMemberResponse body = new GroupMemberResponse(
+                saved.getId(),
+                saved.getGroup().getGroupId(),
+                saved.getUsers().getId(),
+                saved.getRoleInGroup(),
+                saved.getStatus(),
+                saved.getCreatedAt(),
+                saved.getOwnershipPercentage()
         );
-        return ResponseEntity.ok(saved);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(body);
     }
+
 
 
 }
