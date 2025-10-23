6fee5557874de4d8c8822c8e0fb2c9887e195a0b
Khoi4Work <nguyenkhoiforwork2005@gmail.com>
2025-10-24 02:46:22 +0700

    Delete BEdirectory on main

--- App/src/main/java/khoindn/swp391/be/app/model/Request/AddMemberRequest.java
+++
@@ -1,9 +0,0 @@
-package khoindn.swp391.be.app.model.Request;
-
-import lombok.Data;
-@Data
-public class AddMemberRequest {
-    private int userId;
-    private String roleInGroup;
-    private Float ownershipPercentage;
-}
