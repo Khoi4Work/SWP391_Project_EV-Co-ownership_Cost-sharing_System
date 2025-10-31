fd4d4f25407ffdfb762a2a0c98c8419224d1e8fe
Ngocduy222 <duy1006z@gmail.com>
2025-10-31 13:59:08 +0700

    Update GroupDetail.tsx
    change color

--- src/pages/co-owner/GroupDetail.tsx
+++ src/pages/co-owner/GroupDetail.tsx
@@ -550,7 +550,7 @@ export default function GroupDetail() {
                                             <Badge
                                                 className={
                                                     usage.status === "Chờ nhận xe"
-                                                        ? "bg-green-100 text-green-700 border-green-200"
+                                                        ? "bg-blue-100 text-blue-700 border-blue-200"
                                                         : usage.status === "Đang sử dụng"
                                                             ? "bg-orange-100 text-orange-700 border-orange-200"
                                                             : "bg-emerald-100 text-emerald-700 border-emerald-200"
@@ -675,7 +675,7 @@ export default function GroupDetail() {
                                     <Badge
                                         className={
                                             selectedHistory.status === "Chờ nhận xe"
-                                                ? "bg-green-100 text-green-700 border-green-200"
+                                                ? "bg-blue-100 text-blue-700 border-blue-200"
                                                 : selectedHistory.status === "Đang sử dụng"
                                                     ? "bg-orange-100 text-orange-700 border-orange-200"
                                                     : "bg-emerald-100 text-emerald-700 border-emerald-200"
