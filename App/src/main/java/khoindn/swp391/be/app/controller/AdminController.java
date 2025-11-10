4e9f4f0d5f56d8df2d083b5a06ac5c30c51d9000
LAMVANTUAN\ADMIN <tlamvantuan@gmail.com>
2025-11-10 23:49:45 +0700

    i commit at 11:49 uncommand code

--- App/src/main/java/khoindn/swp391/be/app/controller/AdminController.java
+++ App/src/main/java/khoindn/swp391/be/app/controller/AdminController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin/staff") // Đường dẫn gốc
 @RequiredArgsConstructor
-//@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
     @Autowired
     IUserService iUserService;
