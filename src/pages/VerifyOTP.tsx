b3ab544a9fca13cb7ba0bc9b970d557a3c1a31a7
TriSE184800 <nguyennamminhtri@gmail.com>
2025-11-12 00:02:50 +0700

    switch to BE

--- src/pages/VerifyOTP.tsx
+++ src/pages/VerifyOTP.tsx
@@ -32,7 +32,7 @@ export default function VerifyOTP() {
     }, [userData]);
 
     // 🔹 Frontend tạo OTP và gửi tới backend để backend gửi mail
-    const SEND_OTP = import.meta.env.VITE_SEND_EMAIL_PATH;
+    const SEND_OTP = import.meta.env.VITE_SEND_EMAIL_OTP_PATH;
     const sendOtpEmail = async () => {
         const randomOtp = Math.floor(100000 + Math.random() * 900000).toString();
         setOtp(randomOtp);
